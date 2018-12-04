0ae8cbf5c0b1a198b963490985b7738392ebcb97
ahriman <ahriman@falte.red>
2018-12-03 19:22:25 -0500

    installed dokuwiki, added to navbar, updated news

---
+++ wiki/conf/users.auth.php
@@ -0,0 +1,12 @@
+# users.auth.php
+# <?php exit()?>
+# Don't modify the lines above
+#
+# Userfile
+#
+# Format:
+#
+# login:passwordhash:Real Name:email:groups,comma,separated
+
+
+ahriman:$1$lQneqQgd$dFNTq6hx8rvaYavSImhG/.:ahriman:ahriman@falte.red:admin,user
