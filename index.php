277bde9c3dfee323e1551f58a624e2f1413de62c
gbmor <ahriman@falte.red>
2020-05-14 01:47:29 -0400

    added several new languages:
        apl 1.7
        chicken scheme 5.1.0
        cim simula 5.1
        cython 0.29.13
        fpc pascal 3.0.4
        gambit scheme 4.9.3
        gerbil scheme 0.15.1
        gnucobol 2.2
        gpc pascal 20070904
        gprolog 1.4.5
        janet 1.3.1
        nbfc brainfuck 0.1
        ocaml 4.09.0
        pypy 7.1.1
        sbcl commonlisp 1.5.5
        squeak smalltalk 3.10.1
        swi-prolog 7.6.0
    
    and others

--- index.php
+++ index.php
@@ -51,39 +51,52 @@
             </ul>
             <p>The compilers and interpreters available are:</p>
             <ul>
+		<li><code>apl 1.7</code></li>
                 <li><code>brainf*ck 2.7.1</code></li>
-		<li><code>chicken 5.1.0</code></li>
+		<li><code>chicken (Scheme) 5.1.0</code></li>
+		<li><code>cim (Simula) 5.1</code></li>
                 <li><code>clang 8.0.1</code></li>
                 <li><code>clisp 2.49</code></li>
                 <li><code>clojure 1.10.0.4</code></li>
                 <li><code>crystal 0.30.1</code></li>
+		<li><code>cython 0.29.13 (Py2 and Py3)</code></li>
                 <li><code>elixir 1.9.1</code></li>
                 <li><code>erlang 21.2</code></li>
-		<li><code>gambit 4.9.3</code></li>
+		<li><code>fpc (Pascal) 3.0.4</code></li>
+		<li><code>gambit (Scheme) 4.9.3</code></li>
                 <li><code>gcc 8.3.0 (see the <a href="https://wiki.tilde.institute/w/gcc8">wiki page</a>)</code></li>
-		<li><code>gerbil scheme 0.15.1 (see the <a href="https://wiki.tilde.institute/w/gerbilscheme">wiki page</a>)</code></li>
+		<li><code>gerbil (Scheme) 0.15.1 (see the <a href="https://wiki.tilde.institute/w/gerbilscheme">wiki page</a>)</code></li>
                 <li><code>gforth 0.7.3</code></li>
                 <li><code>gfortran / g95 8.3.0 (see the <a href="https://wiki.tilde.institute/w/gcc8">wiki page</a>)</code></li>
-                <li><code>ghc 8.6.4</code></li>
-                <li><code>gnat 8.3.0</code></li>
-                <li><code>go 1.13.1</code></li>
-                <li><code>guile 1.8.8</code></li>
+                <li><code>ghc (Haskell) 8.6.4</code></li>
+                <li><code>gnat (Ada) 8.3.0</code></li>
+		<li><code>gnucobol 2.2</code></li>
+		<li><code>go 1.13.1</code></li>
+		<li><code>gpc (Pascal) 20070904</code></li>
+		<li><code>gprolog 1.4.5</code></li>
+                <li><code>guile 1.8.8 and 2.2.6</code></li>
 		<li><code>janet 1.3.1</code></li>
                 <li><code>jdk 11 and 8 (see the <a href="https://wiki.tilde.institute/w/jdk">wiki page</a>)</code></li>
                 <li><code>lua 5.3.5 and 5.1.5</code></li>
                 <li><code>mono 5.20.1.34</code></li>
                 <li><code>nasm 2.14.02</code></li>
+		<li><code>nbfc (brainf*ck) 0.1</code></li>
 		<li><code>newlisp 10.7.1</code></li>
                 <li><code>nim 1.0.4</code></li>
                 <li><code>node.js 10.16.3</code></li>
+		<li><code>ocaml 4.09.0</code></li>
                 <li><code>perl 5.28.2</code></li>
                 <li><code>php 7.3.16</code></li>
+		<li><code>pypy 7.1.1</code></li>
                 <li><code>python 2.7.16 and 3.7.4</code></li>
                 <li><code>racket 7.4</code></li>
-                <li><code>rakudo 2018.12</code></li>
+                <li><code>rakudo (Raku / Perl6) 2018.12</code></li>
                 <li><code>ruby 2.5.5 and 2.6.6</code></li>
                 <li><code>rust 1.38</code></li>
-                <li><code>scala 2.12.3</code></li>
+		<li><code>sbcl (Common Lisp) 1.5.5</code></li>
+		<li><code>scala 2.12.3</code></li>
+		<li><code>squeak-vm (Smalltalk) 3.10.1</code></li>
+		<li><code>swi-prolog 7.6.0</code></li>
 		<li><code>tinyscheme 1.41</code></li>
                 <li><code>yasm 1.3.0</code></li>
             </ul>
